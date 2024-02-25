5de40219ea02bc669011f0e77f980472407baefa
Tanutsapon Jinaongkan <tanutsaponjinaongkan@Tanutsapons-MacBook-Air.local>
2024-02-25 15:59:16 -0500

    Refactor navbar component to include mobile menu

--- src/components/navbar/navDesktop/NavbarDesktop.jsx
+++ src/components/navbar/navDesktop/NavbarDesktop.jsx
@@ -3,32 +3,43 @@ import logo from "../../../assets/logo.png";
 
 import { RiMenu4Line } from "react-icons/ri";
 import navItem from "../navLinks/NavLinks";
+import { useState } from "react";
+import NavbarMobile from "../navMobile/NavbarMobile";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(true);
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <div className="bg-background sticky top-0 z-10">
-      <nav className="max-w-screen-xl mx-auto py-4 px-6">
-        <div className="flex items-center justify-between">
-          <img src={logo} className="h-11 w-auto object-contain" alt="logo" />
+    <>
+      {openMenu && <NavbarMobile setOpenMenu={setOpenMenu} />}
+      <div className="bg-background sticky top-0 z-10">
+        <nav className="max-w-screen-xl mx-auto py-4 px-6">
+          <div className="flex items-center justify-between">
+            <img src={logo} className="h-11 w-auto object-contain" alt="logo" />
 
-          <ul className="hidden md:flex md:gap-14">
-            {navItem.map((nav) => (
-              <li key={nav.id}>
-                <Link className="menu-item">{nav.title}</Link>
-              </li>
-            ))}
-          </ul>
+            <ul className="hidden md:flex md:gap-14">
+              {navItem.map((nav) => (
+                <li key={nav.id}>
+                  <Link className="menu-item">{nav.title}</Link>
+                </li>
+              ))}
+            </ul>
 
-          <button className="hidden h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block">
-            Contact Us
-          </button>
+            <button className="hidden h-10 bg-primary text-white text-sm px-6 rounded hover:bg-blue-200 hover:text-primary md:block">
+              Contact Us
+            </button>
 
-          <button className="w-11 h-11 bg-blue-100 text-2xl text-primary flex items-center justify-center rounded md:hidden z-50">
-            <RiMenu4Line />
-          </button>
-        </div>
-      </nav>
-    </div>
+            <button className="w-11 h-11 bg-blue-100 text-2xl text-primary flex items-center justify-center rounded md:hidden z-50">
+              <RiMenu4Line />
+            </button>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
--- src/components/navbar/navMobile/NavbarMobile.jsx
+++ src/components/navbar/navMobile/NavbarMobile.jsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 import navItem from "../navLinks/NavLinks";
 
-const NavbarMobile = () => {
+const NavbarMobile = ({ setOpenMenu }) => {
   return (
-    <div>
-      <div>
-        <img src={logo} alt="logo" className="object-contain mb-8" />
+    <div className="w-screen fixed top-0 z-20">
+      <div className="w-1/2 h-screen p-8 flex flex-col bg-background">
+        <img src={logo} alt="logo" className="w-16 object-contain mb-8" />
 
         <ul>
           {navItem.map((nav) => (
-            <li key={nav.id}>
-              <Link>{nav.title}</Link>
+            <li key={nav.id} className="mb-5">
+              <Link className="menu-item">{nav.title}</Link>
             </li>
           ))}
         </ul>
 
-        <button>Contact Us</button>
+        <button className="h-10 text-white bg-primary">Contact Us</button>
       </div>
     </div>
   );
